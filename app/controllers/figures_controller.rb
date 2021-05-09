6c6244f39e4efe24af489f459617de775210831e
George Dilthey <george.dilthey@gmail.com>
2021-05-09 16:16:35 -0400

    commit tree 347ae724c83780d3fc129070f0594fab1f19b075

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,66 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+  
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    figure = Figure.create(:name => params[:figure][:name])
+    if params[:title][:name].empty?
+      figure.title_ids = params[:figure][:title_ids]
+    else 
+      figure.title_ids = Title.create(:name => params[:title][:name]).id
+    end
+
+    if params[:landmark][:name].empty?
+      figure.landmark_ids = params[:figure][:landmark_ids]
+    else 
+      figure.landmark_ids = Landmark.create(:name => params[:landmark][:name]).id
+    end
+
+    redirect "/figures/#{figure.id}"
+
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    figure = Figure.find_by_id(params[:id])
+
+    figure.update(:name => params[:figure][:name])
+
+    if params[:title][:name].empty?
+      figure.title_ids = params[:figure][:title_ids]
+    else 
+      figure.title_ids = Title.create(:name => params[:title][:name]).id
+    end
+
+    if params[:landmark][:name].empty?
+      figure.landmark_ids = params[:figure][:landmark_ids]
+    else 
+      figure.landmark_ids = Landmark.create(:name => params[:landmark][:name]).id
+    end
+  
+    redirect "/figures/#{figure.id}"
+  end
+
 end
